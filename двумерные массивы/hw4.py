5aadb6fed4f4ef12fc9004e58d724acd87b7a761
djfvjf1 <86651267+djfvjf1@users.noreply.github.com>
2022-10-27 18:52:37 +0600

    Update hw4.py

--- двумерные массивы/hw4.py
+++ двумерные массивы/hw4.py
@@ -1,20 +1,16 @@
-n = [int(x) for x in input().split()]
-a = [[0] * int(n[1]) for i in range(n[0])]
+n = [int(i) for i in input().split()]
+a = [[0] * n[1] for i in range(n[0])]
 
-for i in range(n):
+for i in range(len(a)):
     for j in range(len(a[i])):
-        a[i][j]=a[i-1][j]+a[i][j-1]
+       a[0][j] = 1
+       a[i][0] = 1
+       a[i][j] = a[i - 1][j] + a[i][j - 1]
 
-
-for i in range(n): 
-
-    for j in range(len(a[i])): 
-
-        a[i][j] = str(a[i][j]) 
- 
-
-# Записываем массив в виде матрицы
-for d in range(len(a)): 
-    
-    s = " ".join(a[d]) 
-    print(s) 
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        a[i][j] = str(a[i][j])
+        
+for d in range(len(a)):
+    s = " ".join(a[d])
+    print(s)
